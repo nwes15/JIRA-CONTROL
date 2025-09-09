5cccd595303862969d33ae4abd76e170801d4a4f
Wesley Novais <109444226+nwes15@users.noreply.github.com>
2025-09-09 18:57:46 -0300

    fix

--- jira_project/admin.py
+++ jira_project/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from .models import Customer, Ticket, Jira, Comment
 
-admin.site.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
 
-admin.site.register(Ticket)
 class TicketAdmin(admin.ModelAdmin):
     list_display = ('number', 'customer')
     search_fields = ('number', 'customer__name')
@@ -17,9 +15,13 @@ class CommentInline(admin.TabularInline):
     extra = 1
     fields = ('author', 'text', 'informed_date')
 
-admin.site.register(Jira)
 class JiraAdmin(admin.ModelAdmin):
     list_display = ('jira_number', 'type', 'creation_date')
     search_fields = ('jira_number',)
     list_filter = ('type',)
     filter_horizontal = ('tickets',)
+    inlines = [CommentInline]
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(Jira, JiraAdmin)
